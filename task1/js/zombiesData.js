f2e5dbf114f1855c6bd9d99f037914ba59740537
valeryvarantsou <valery.vorontsov@solbeg.com>
2020-02-02 17:31:13 +0300

    task1 first commit

---
+++ task1/js/zombiesData.js
@@ -0,0 +1,14 @@
+const zombiesData = [];
+
+zombiesData[0] = 20;
+zombiesData[1] = 1;
+zombiesData[2] = -2;
+zombiesData[3] = 0;
+zombiesData[5] = 11;
+zombiesData[6] = 'a';
+zombiesData[7] = '-40';
+zombiesData[8] = 4;
+zombiesData[9] = '0';
+zombiesData[10] = null;
+zombiesData[11] = 24;
+zombiesData[12] = 10;
