7eedab6e9e98317b21e101792d2408fb9b335c1e
valeryvarantsou <valery.vorontsov@solbeg.com>
2020-02-21 14:06:52 +0300

    [task2] - implementation without changing array

--- Task2/js/app.js
+++ Task2/js/app.js
@@ -1,37 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    ZOMBIE_TYPE.DEAD = 0;
-
     const zombieList = document.querySelector('.zombies');
 
-    renderZombieList(zombies);
-
-    function renderZombieList(units) {
-        zombieList.innerHTML = '';
-
-        for (let i = 0; i < units.length; i++) {
-            renderZombie(units[i], i);
-        }
-    }
-
-    function renderZombie(item, index) {
+    for (let i = 0; i < zombies.length; i++) {
         const zombie = document.createElement('div');
 
         zombie.classList.add('zombie');
 
-        if (item.type === ZOMBIE_TYPE.SMALL) {
+        if (zombies[i].type === ZOMBIE_TYPE.SMALL) {
             zombie.classList.add('zombie--small');
-        } else if (item.type === ZOMBIE_TYPE.MAD) {
+        } else if (zombies[i].type === ZOMBIE_TYPE.MAD) {
             zombie.classList.add('zombie--mad');
-        } else if (item.type === ZOMBIE_TYPE.STRONG) {
+        } else if (zombies[i].type === ZOMBIE_TYPE.STRONG) {
             zombie.classList.add('zombie--strong');
-        } else if (item.type === ZOMBIE_TYPE.DEAD) {
-            zombie.classList.add('zombie--dead');
         }
 
         zombie.addEventListener('click', function (event) {
-            if (item.type !== ZOMBIE_TYPE.DEAD) {
-                zombies[index].type = 0;
-                renderZombieList(zombies);
+            if (!event.target.classList.contains('zombie--dead')) {
+                event.target.classList.value = 'zombie zombie--dead';
             }
         });
 
